89853d0883c073d9c39a836f6db00a3cdb5cd76e
Gustav <gkhaavik@gmail.com>
2021-01-15 21:27:47 +0100

    FIXED errors and issues #4

--- src/main/java/com/wyvencraft/api/menus/CustomItem.java
+++ src/main/java/com/wyvencraft/api/menus/CustomItem.java
@@ -1,12 +1,5 @@
 package com.wyvencraft.api.menus;
 
-import io.github.portlek.bukkititembuilder.ItemStackBuilder;
-import io.github.portlek.smartinventory.util.SlotPos;
-
-import java.util.List;
-
-public class CustomItem extends IMenuItem {
-    public CustomItem(ItemStackBuilder stack, SlotPos slot, boolean fill, List<SlotPos> slots, List<String> actions, Type type) {
-        super(stack, slot, fill, slots, actions, type);
-    }
+public interface CustomItem extends IMenuItem {
+    
 }
