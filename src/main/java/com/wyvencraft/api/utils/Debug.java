685205ae17f9e1798930af0d30f316535421f077
Gustav <gkhaavik@gmail.com>
2021-03-02 11:55:56 +0100

    added debug.log

--- src/main/java/com/wyvencraft/api/utils/Debug.java
+++ src/main/java/com/wyvencraft/api/utils/Debug.java
@@ -4,4 +4,8 @@ public class Debug {
     public static void log(Class<?> c, String msg) {
         System.out.println("[DEBUG-" + c.getName() + "] " + msg);
     }
+
+    public static void log(String msg) {
+        System.out.println("[DEBUG] " + msg);
+    }
 }
